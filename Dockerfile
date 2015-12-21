9e95c68892d27cc13fac193b1768bd4690768b5d
Kha <nlhkha@gmail.com>
2015-12-21 09:47:01 +0700

    include extension

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ MAINTAINER Kha Nguyen <kha@myladybug.co>
 RUN mkdir -p /tmp/letsencrypt/public_html/.well-known/acme-challenge
 COPY run.sh /tmp/letsencrypt/public_html/run.sh
 WORKDIR /tmp/letsencrypt/public_html
-ENV LETS_ENCRYPT_CHALLENGE_KEY please_change_me
+ENV CHALLENGE_KEY please_change_me
+ENV EXTENSION change_me
 EXPOSE 80
 ENTRYPOINT ./run.sh
--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-rm ${LETS_ENCRYPT_CHALLENGE_KEY}
-printf "%s" ${LETS_ENCRYPT_CHALLENGE_KEY} > .well-known/acme-challenge/${LETS_ENCRYPT_CHALLENGE_KEY}
+rm ${CHALLENGE_KEY}
+printf "%s" ${CHALLENGE_KEY}.${EXTENSION} > .well-known/acme-challenge/${CHALLENGE_KEY}
 
 $(command -v python2 || command -v python2.7 || command -v python2.6) -c \
 "import BaseHTTPServer, SimpleHTTPServer; \
 s = BaseHTTPServer.HTTPServer(('', 80), SimpleHTTPServer.SimpleHTTPRequestHandler); \
 s.serve_forever()"
-
