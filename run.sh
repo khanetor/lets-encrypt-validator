e781791aaf3ca65525cc40d1e736e5e987ba0152
Kha <nlhkha@gmail.com>
2016-03-15 08:11:47 +0700

    have 2 keys

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm ${CHALLENGE_KEY}
-printf "%s" ${CHALLENGE_KEY}.${EXTENSION} > .well-known/acme-challenge/${CHALLENGE_KEY}
+printf "%s" ${CHALLENGE_KEY1}.${EXTENSION1} > .well-known/acme-challenge/${CHALLENGE_KEY1}
+printf "%s" ${CHALLENGE_KEY2}.${EXTENSION2} > .well-known/acme-challenge/${CHALLENGE_KEY2}
 
 $(command -v python2 || command -v python2.7 || command -v python2.6) -c \
 "import BaseHTTPServer, SimpleHTTPServer; \
